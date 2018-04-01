1331463ac981a57777b071d8575cf4ef429fe18b
Andrey Smirnov <37780231+Adisey@users.noreply.github.com>
2018-04-02 01:48:28 +0300

    Save the working day result

--- main.js
+++ main.js
@@ -6,27 +6,49 @@ window.onload = function () {
     var slides = document.querySelectorAll('#slides .slide');
     var currentSlide = 0;
     var slideInterval = setInterval(nextSlide, 2000);
+    var playing = true;
+    var pauseButton = document.getElementById('pause');
+    var next = document.getElementById('next');
+    var previous = document.getElementById('previous');
+
+    function displayFile(sv) {
+        var tempName = sv.innerHTML;
+        tempName = tempName.slice(tempName.indexOf('"') + 1);
+        tempName = tempName.slice(0, tempName.lastIndexOf('"'));
+        document.getElementById('fileName').innerHTML = tempName;
+
+    }
 
     function nextSlide() {
         slides[currentSlide].className = 'slide';
         currentSlide = (currentSlide + 1) % slides.length;
         slides[currentSlide].className = 'slide showing';
-    }
+        displayFile(slides[currentSlide]);
+    };
 
-    var playing = true;
-    var pauseButton = document.getElementById('pause');
 
     function pauseSlideshow() {
-        pauseButton.innerHTML = 'Play';
+        // var button = document.getElementById("pause");
+        // var style = button[].style;
+        // style.backgroundImage  = "url('pic/play.png')";
+        pauseButton.style.backgroundImage = 'url("pic/play.png")';
+        // image.src='pic/play.png';
+        pauseButton.title = 'Play';
+        // pauseButton.innerHTML = 'Play';
+        console.log(pauseButton);
         playing = false;
         clearInterval(slideInterval);
-    }
+    };
 
     function playSlideshow() {
-        pauseButton.innerHTML = 'Pause';
+        // var image=document.getElementById("pause");
+        // image.src='pic/pause.png';
+        pauseButton.style.backgroundImage = 'url("pic/pause.png")';
+        pauseButton.title = 'Pause';
+        // pauseButton.innerHTML = 'Pause';
         playing = true;
         slideInterval = setInterval(nextSlide, 2000);
-    }
+    };
 
     pause.onclick = function () {
         if (playing) {
@@ -36,4 +58,28 @@ window.onload = function () {
         }
     };
 
-}
+    function nextSlide() {
+        goToSlide(currentSlide + 1);
+    };
+
+    function previousSlide() {
+        goToSlide(currentSlide - 1);
+    };
+
+    function goToSlide(n) {
+        slides[currentSlide].className = 'slide';
+        currentSlide = (n + slides.length) % slides.length;
+        slides[currentSlide].className = 'slide showing';
+        displayFile(slides[currentSlide]);
+    };
+
+    next.onclick = function () {
+        pauseSlideshow();
+        nextSlide();
+    };
+    previous.onclick = function () {
+        pauseSlideshow();
+        previousSlide();
+    };
+
+};
